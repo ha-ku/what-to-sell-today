500ec2b30ab3be8ae47a9271cb5c8a89442f67a0
haku <haku@protonmail.com>
2022-01-24 13:30:53 +0800

    Add PWA support

--- pages/_document.js
+++ pages/_document.js
@@ -18,23 +18,23 @@ export default class MyDocument extends Document {
             <meta name='description' content='Frontend and backend to compare FFXIV item price in various list.' />
             <meta name='format-detection' content='telephone=no' />
             <meta name='mobile-web-app-capable' content='yes' />
-            <!-- <meta name='msapplication-config' content='/icons/browserconfig.xml' /> -->
+            {/* <meta name='msapplication-config' content='/icons/browserconfig.xml' /> */}
             <meta name='msapplication-TileColor' content='#64b5f6' />
             <meta name='msapplication-tap-highlight' content='no' />
             <meta name='theme-color' content='#64b5f6' />
 
-            <!--
+            {/*
             <link rel='apple-touch-icon' href='/icons/touch-icon-iphone.png' />
             <link rel='apple-touch-icon' sizes='152x152' href='/icons/touch-icon-ipad.png' />
             <link rel='apple-touch-icon' sizes='180x180' href='/icons/touch-icon-iphone-retina.png' />
             <link rel='apple-touch-icon' sizes='167x167' href='/icons/touch-icon-ipad-retina.png' />
-            -->
+            */}
 
-            <!--
+            {/*
             <link rel='icon' type='image/png' sizes='32x32' href='/icons/favicon-32x32.png' />
             <link rel='icon' type='image/png' sizes='16x16' href='/icons/favicon-16x16.png' />
             <link rel='mask-icon' href='/icons/safari-pinned-tab.svg' color='#5bbad5' />
-            -->
+            */}
             <link rel='manifest' href='/manifest.json' />
             <link rel='shortcut icon' href='/favicon.ico' />
             <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto:300,400,500' />
@@ -50,10 +50,10 @@ export default class MyDocument extends Document {
             <meta property='og:description' content='Frontend and backend to compare FFXIV item price in various list.' />
             <meta property='og:site_name' content='What To Sell Today' />
             <meta property='og:url' content='https://what-to-sell-today.vercel.com' />
-            <!--<meta property='og:image' content='https://yourdomain.com/icons/apple-touch-icon.png' />-->
+            {/*<meta property='og:image' content='https://yourdomain.com/icons/apple-touch-icon.png' />*/}
 
-            <!-- apple splash screen images -->
-            <!--
+            {/* apple splash screen images */}
+            {/*
 			<link rel='apple-touch-startup-image' href='/images/apple_splash_2048.png' sizes='2048x2732' />
 			<link rel='apple-touch-startup-image' href='/images/apple_splash_1668.png' sizes='1668x2224' />
 			<link rel='apple-touch-startup-image' href='/images/apple_splash_1536.png' sizes='1536x2048' />
@@ -61,7 +61,7 @@ export default class MyDocument extends Document {
 			<link rel='apple-touch-startup-image' href='/images/apple_splash_1242.png' sizes='1242x2208' />
 			<link rel='apple-touch-startup-image' href='/images/apple_splash_750.png' sizes='750x1334' />
 			<link rel='apple-touch-startup-image' href='/images/apple_splash_640.png' sizes='640x1136' />
-			-->
+			*/}
           </Head>
           <body>
           <Main />
