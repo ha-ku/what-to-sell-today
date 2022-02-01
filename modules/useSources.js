d6fab28fd5188f400eac7a7891e274d3ba2a302a
haku <haku@protonmail.com>
2022-02-01 18:41:02 +0800

    Fix initial source state.

--- modules/useSources.js
+++ modules/useSources.js
@@ -1,16 +1,26 @@
 import { useState, useEffect } from "react";
 
 function useSources(shouldStart, setError) {
-	const [JSONSources, setJSONSources] = useState({});
+	const [JSONSources, setJSONSources] = useState({
+		companySealList: [],
+		botanyList: [],
+		miningList: [],
+		fishList: [],
+		huntingList: [],
+		dyeList: [],
+		mapList: [],
+		allegoryList: [],
+		yellowGathererScripsList: []
+	});
 	const sources = {
-		companySeal: {target: "军票", action: "换", withTime: false, source: JSONSources.companySealList ?? [], category: "货币"},
-		botany: {target: "伐木", action: "砍", withTime: true, source: JSONSources.botanyList ?? [], category: "雇员"},
-		mining: {target: "采矿", action: "挖", withTime: true, source: JSONSources.miningList ?? [], category: "雇员"},
-		fish: {target: "捕鱼", action: "钓", withTime: true, source: JSONSources.fishList ?? [], category: "雇员"},
-		hunting: {target: "战职", action: "带", withTime: true, source: JSONSources.huntingList ?? [], category: "雇员"},
-		dye: {target: "染剂", action: "搓", withTime: false, source: JSONSources.dyeList ?? [], category: "生产"},
-		map: {target: "宝图", action: "挖", withTime: false, source: JSONSources.mapList ?? [], category: "采集"},
-		allegory: {target: "奇谭", action: "换", withTime: false, source: JSONSources.allegoryList ?? [], category: "货币"},
+		companySeal: {target: "军票", action: "换", withTime: false, source: JSONSources.companySealList, category: "货币"},
+		botany: {target: "伐木", action: "砍", withTime: true, source: JSONSources.botanyList, category: "雇员"},
+		mining: {target: "采矿", action: "挖", withTime: true, source: JSONSources.miningList, category: "雇员"},
+		fish: {target: "捕鱼", action: "钓", withTime: true, source: JSONSources.fishList, category: "雇员"},
+		hunting: {target: "战职", action: "带", withTime: true, source: JSONSources.huntingList, category: "雇员"},
+		dye: {target: "染剂", action: "搓", withTime: false, source: JSONSources.dyeList, category: "生产"},
+		map: {target: "宝图", action: "挖", withTime: false, source: JSONSources.mapList, category: "采集"},
+		allegory: {target: "奇谭", action: "换", withTime: false, source: JSONSources.allegoryList, category: "货币"},
 		yellowGathererScrips: {target: "黄票", action: "换", withTime: false, source: JSONSources.yellowGathererScripsList, category: "采集"}
 	};
 	useEffect(() => {
