f3d51d8729c8498f4a81b5c464888de6e8fc9880
haku <haku@protonmail.com>
2022-02-28 21:11:54 +0800

    Smoothen line chart

--- modules/LineChart.js
+++ modules/LineChart.js
@@ -11,7 +11,17 @@ const LineChart = function({width, height, data, color, darkMode}) {
 	const area = useMemo(() => {
 		const maxY = Math.max(...(data.map(([_,y]) => y)));
 		const maxX = Math.max(...(data.map(([x,_]) => x)));
-		const path = maxY === 0 ? 'M 0 0' : data.map((point, i) => `${i === 0 ? 'M' : 'L'} ${point[0] / maxX * width} ${point[1] / maxY * height}`).join(' ');
+		const scaleX = x => (x / maxX * width).toFixed(2);
+		const scaleY = y => (y / maxY * height).toFixed(2);
+		const path = maxY === 0 ?'M 0 0' : data.map((point, i) =>
+			i === 0 ? `M ${scaleX(point[0])} ${scaleY(point[1])}`
+				: (i === data.length - 1 ? `L ${scaleX(point[0])} ${scaleY(point[1])}`
+					: (i === 1 ? `Q ${scaleX(point[0])} ${scaleY(point[1])} ${scaleX((point[0]+data[i+1][0]) / 2)} ${scaleY((point[1] + data[i+1][1]) / 2)}`
+						: `T ${scaleX((point[0]+data[i+1][0]) / 2)} ${scaleY((point[1] + data[i+1][1]) / 2)}`
+					)
+				)
+			//`${i === 0 ? 'M' : 'L'} ${point[0] / maxX * width} ${point[1] / maxY * height}`
+		).join(' ');
 		return path + ` L ${width} 0 L 0 0 Z`;
 	}, [data, width, height])
 
